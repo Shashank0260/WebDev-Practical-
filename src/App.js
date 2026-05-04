21842fa7d2d363a4ee009b868243ccb79054fecc
Shashank <shashank.260307@gmail.com>
2026-05-04 20:32:30 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,54 +1,28 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [data, setData] = useState([]);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
-
-  const decrement = () => {
-    setCount(count - 1);
-  };
-
-  const reset = () => {
-    setCount(0);
-  };
+  useEffect(() => {
+    axios.get("https://jsonplaceholder.typicode.com/albums")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   return (
-    <div style={styles.container}>
-      <h1>Counter App</h1>
+    <div style={{ textAlign: "center" }}>
+      <h1>Albums</h1>
 
-      <h2>Count: {count}</h2>
-
-      <div>
-        <button onClick={increment} style={styles.button}>
-          Increment
-        </button>
-
-        <button onClick={decrement} style={styles.button}>
-          Decrement
-        </button>
-
-        <button onClick={reset} style={styles.button}>
-          Reset
-        </button>
-      </div>
+      {data.slice(0, 5).map((item) => (
+        <p key={item.id}>{item.title}</p>
+      ))}
     </div>
   );
 }
-
-const styles = {
-  container: {
-    textAlign: "center",
-    marginTop: "50px",
-  },
-  button: {
-    margin: "10px",
-    padding: "10px 20px",
-    fontSize: "16px",
-    cursor: "pointer",
-  },
-};
 
 export default App;
